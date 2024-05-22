757da831656a4d034d68fffb96b6ed3fd2f3b101
HeatMeansen <kotk6509@gmail.com>
2024-05-22 16:43:07 +0300

    Add files via upload

---
+++ sc.bat
@@ -0,0 +1,3 @@
+@echo off
+xcopy /? > C:\labs\pz-23-2\Kotov\batch\NotHidden\copyhelp.txt
+xcopy C:\labs\pz-23-2\Kotov\batch\NotHidden\copyhelp.txt C:\labs\pz-23-2\Kotov\batch\Hidden\copied_copyhelp.txt
